c0893ac73a5049a6879280515da024cbe06d85d6
lockdain <asergeenko@neoflex.ru>
2022-01-03 01:01:38 +0300

    Fix analysis issues

--- src/main/scala/ru/asergeenko/schrandom/validator/MessageValidator.scala
+++ src/main/scala/ru/asergeenko/schrandom/validator/MessageValidator.scala
@@ -3,7 +3,9 @@ package ru.asergeenko.schrandom.validator
 import org.apache.avro.Schema
 import org.apache.avro.specific.SpecificRecordBase
 
+import scala.util.Try
+
 trait MessageValidator {
-  def validateJson(schema: Schema, message: String): Boolean
-  def validateAvro(schema: Schema, message: SpecificRecordBase): Boolean
+  def validateJson(schema: Schema, message: String): Try[Boolean]
+  def validateAvro(schema: Schema, message: SpecificRecordBase): Try[Boolean]
 }
