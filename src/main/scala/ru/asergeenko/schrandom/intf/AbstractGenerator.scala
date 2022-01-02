ef696dce0450bbcdbae4f3fefc5d37c771383f26
lockdain <asergeenko@neoflex.ru>
2022-01-03 01:07:00 +0300

    Fix analysis issues

--- src/main/scala/ru/asergeenko/schrandom/intf/AbstractGenerator.scala
+++ src/main/scala/ru/asergeenko/schrandom/intf/AbstractGenerator.scala
@@ -3,6 +3,6 @@ package ru.asergeenko.schrandom.intf
 import ru.asergeenko.schrandom.settings.GeneratorBehavior
 
 trait AbstractGenerator extends Runnable {
-  def run
-  def getBehavior(): GeneratorBehavior
+  def run()
+  def getBehavior: GeneratorBehavior
 }
